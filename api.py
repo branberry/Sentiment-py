6827d2dbf646672ca0713f2f6a69bd298e324123
wowbob396 <wowbob396@gmail.com>
2018-07-09 17:31:36 -0500

    first commit

---
+++ api.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class TestModel(Resource):
+    def post(self):
+        return {}
+        
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
